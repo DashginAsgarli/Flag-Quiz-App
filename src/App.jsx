97a898f7df1819de63b236652e89af797c2fdaea
DashginAsgarli <dashqinasgarli@gmail.com>
2026-02-05 01:48:44 +0400

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link ,NavLink } from 'react-router-dom';
 import Header from './component/Header/header.jsx';
+
+import HomePage from './component/Main/HomePage.jsx';
+import Profil from './component/Main/profil.jsx';
+import Shop from './component/Main/shop.jsx';
+import Experience from './component/Main/experience.jsx';
+import Rating from './component/Main/rating.jsx';
+
 function App() {
   return (
     <>
-    <Header />
-      
+      <Header />
+
+
+
+
+      <section>
+        <Routes>
+          <Route path= "/" element={<HomePage />} />
+          <Route path= "/profil" element={<Profil />} />
+          <Route path= "/shop" element={<Shop />} />
+          <Route path= "/experience" element={<Experience />} />
+          <Route path= "/rating" element={<Rating />} />
+        </Routes>
+      </section>
+
     </>
   )
 }
